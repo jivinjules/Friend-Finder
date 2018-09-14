3c5f3206e3754576554087e44d82e941ab181606
jivinjules <juliehodges05@gmail.com>
2018-09-14 13:15:38 -0500

    html route update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,12 +3,12 @@
    module.exports = function(app) {
     // Home page
 	app.get('/home', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/home.html'));
+		res.sendFile(path.join(__dirname, '/../public/home.html'));
 	});
 
 	// Survey page
 	app.get('/survey', function(req, res) {
-		res.sendFile(path.join(__dirname, '../public/survey.html'));
+		res.sendFile(path.join(__dirname, '/../public/survey.html'));
 		});
 		
 
